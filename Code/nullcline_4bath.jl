56d689311542c57810b6c6e6348f883f3e46b2db
jacobcook1995 <jc2017@ic.ac.uk>
2018-01-31 15:18:02 +0000

    Got something that basically works

--- Code/nullcline_4bath.jl
+++ Code/nullcline_4bath.jl
@@ -27,85 +27,54 @@ const N = 2000.0 # density within the system
 
 # functions to calculate the nullclines for A at a given point
 # x[1] = A, x[2] = B, x[3] = W, x[4] = S
-function nullclineS1(x)
-    S1 = (r + f*x[2]^2)*((kmin + K)*x[1] - Kmin*x[3])/(k*r)
-    return(S1)
+function nullclineA(x)
+    A = (k*x[4]*r/(r + f*x[2]^2) + Kmin*x[3])/(kmin + K)
+    return(A)
 end
 
-function nullclineS2(x)
-    S2 = (r + f*x[1]^2)*((kmin + K)*x[2] - Kmin*x[3])/(k*r)
-    return(S2)
+function nullclineB(x)
+    B = (k*x[4]*r/(r + f*x[1]^2) + Kmin*x[3])/(kmin + K)
+    return(B)
 end
 
-function nullclineS3(x)
-    S3 = (kmin*(x[1] + ϕ*x[2]) + F)/(k*(r/(r + f*x[2]^2) + ϕ*r/(r + f*x[1]^2)))
-    return(S3)
+function Wdot(x)
+    wdot = K*(x[1] + ϕ*x[2]) - Kmin*(1 + ϕ)*x[3] - F
+    return(wdot)
 end
 
-function nullclineW1(x)
-    W1 = k*x[4]*r/(Kmin*(r + f*x[2]^2)) - (kmin + K)*x[1]/Kmin
-    return(W1)
-end
-
-function nullclineW2(x)
-    W2 = k*x[4]*r/(Kmin*(r + f*x[1]^2)) - (kmin + K)*x[2]/Kmin
-    return(W2)
-end
-
-function nullclineW3(x)
-    W3 = (K*(x[1] + ϕ*x[2]) - F)/(Kmin*(1 + ϕ))
-    return(W3)
+function nullclineS(x)
+    S = (kmin*(x[1] + ϕ*x[2]) + F)/(k*(r/(r + f*x[2]^2) + ϕ*r/(r + f*x[1]^2)))
+    return(S)
 end
 
 # single function containing all the equations to minimize
 # x[1] = A, x[2] = B, x[3] = W, x[4] = S
 # function termed F1 to avoid name space conflict with global constant F
 function f!(F1, x)
-    F1[1] = nullclineS1(x) - x[4]
-    F1[2] = nullclineS2(x) - x[4]
-    F1[3] = nullclineS3(x) - x[4]
-    F1[4] = nullclineW1(x) - x[3]
-    F1[5] = nullclineW2(x) - x[3]
-    F1[6] = nullclineW3(x) - x[3]
-    F1[7] = N - x[1] - x[2] - x[3] - x[4] # constraint term
+    F1[1] = nullclineA(x) - x[1]
+    F1[2] = nullclineB(x) - x[2]
+    F1[3] = nullclineS(x) - x[4]
+    F1[4] = x[1] + x[2] + x[3] + x[4] - N # constraint term
 end
 
 
 
 function find_zeros()
     # system of 7 equations to find the minimum of
-    xs = nlsolve(f!, [100.0, 700.0, 600.0, 600.0, 0.0, 0.0, 0.0], iterations = 1000) # three zeros are for unused variables
+
     # AiBi = N/10
     # Bi = AiBi/10
     # Ai = AiBi - Bi
     # Si = (N - AiBi)/10
     # Wi = 9*(N - AiBi)/10
-    # xs = nlsolve(f!, [Ai, Bi, Si, Wi, N])
-    print(xs)
-    print("\n")
-    n1 = nullclineS1(xs.zero) - xs.zero[4]
-    n2 = nullclineS2(xs.zero) - xs.zero[4]
-    n3 = nullclineS3(xs.zero) - xs.zero[4]
-    n4 = nullclineW1(xs.zero) - xs.zero[3]
-    n5 = nullclineW2(xs.zero) - xs.zero[3]
-    n6 = nullclineW3(xs.zero) - xs.zero[3]
-    n7 = N - xs.zero[1] - xs.zero[2] - xs.zero[3] - xs.zero[4]
-    print(n1)
-    print("\n")
-    print(n2)
-    print("\n")
-    print(n3)
-    print("\n")
-    print(n4)
-    print("\n")
-    print(n5)
-    print("\n")
-    print(n6)
-    print("\n")
-    print(n7)
-    print("\n")
-    print(n1 + n2 + n3 + n4 + n5 + n6 + n7)
-    print("\n")
+
+    xs = nlsolve(f!, [Ai, Bi, Si, Wi])
+    nW = Wdot(xs.zero)
+    if mag(nW) <= 10.0^-10 && converged(xs) == true
+
+    else
+        error("Error: Not a valid point")
+    end
 end
 
 @time find_zeros()

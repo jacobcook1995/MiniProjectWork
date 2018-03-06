fd1ad7646ca329315b752520f162e3bc08fee398
jacobcook1995 <jc2017@ic.ac.uk>
2018-03-06 13:40:02 +0000

    Starting playing with linear algebra

---
+++ Code/MatrixFun.jl
@@ -0,0 +1,50 @@
+#!/usr/bin/env julia
+# MatrixFun.jl
+# A script to test matrix algebra in julia hopefully developing to the point
+# where I can write a function to calculate the diffusion matrix for arbitary noise
+
+using Plots
+using Roots
+import GR # Need this to stop world age plotting error?
+
+# Firstly should define constants
+const Ω = 300
+const K = 10
+const k = K*Ω # steady state for A=k/K=1
+const Q = 1
+const q = Q*Ω
+const kmin = 10.0^-20 # set all too 10.0^-20 for now
+const Kmin = 10.0^-20
+const qmin = 10.0^-20
+const Qmin = 10.0^-20
+const f = 1000/(Ω^2) # Promoter switching
+const r = 10
+const F = 250 # removal rate
+
+# Multiplicative Guassian noise matrix
+# A = x[1], B = x[2], W = x[3], S = x[4]
+function e!(E, x)
+    E[1,2:4] = E[2,1] = E[2,3:4] = E[3,4] = E[4,3] = 0
+    E[1,1] = sqrt(k*x[4]*r/(r + f*x[2]^2) + K*x[1] + kmin*x[1] + Kmin*x[3]) #gA
+    E[2,2] = sqrt(q*x[4]*r/(r + f*x[1]^2) + Q*x[2] + qmin*x[2] + Qmin*x[3]) #gB
+    E[3,1] = -sqrt(K*x[1] + Kmin*x[3]) #-gWA
+    E[3,2] = -sqrt(Q*x[2] + Qmin*x[3]) #-gWB
+    E[3,3] = sqrt(F) #gW
+    E[4,1] = -sqrt(k*x[4]*r/(r + f*x[2]) + kmin*x[1]) #-gSA
+    E[4,2] = -sqrt(q*x[4]*r/(r + f*x[1]) + qmin*x[2]) #-gSB
+    E[4,4] = sqrt(F) #gS
+    return E
+end
+
+function main()
+    # Gonna try numerically first
+    e = [ 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 ]
+    e = e!(e, [ 10; 10; 10; 10 ])
+    print(e)
+    print("\n")
+    einv = inv(e)
+    print(einv)
+    print("\n")
+end
+
+@time main()

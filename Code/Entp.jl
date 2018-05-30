6e5162b89af77e77f9a03c51f6679f97bdb74f66
jacobcook1995 <jc2017@ic.ac.uk>
2018-05-30 17:25:55 +0100

    Did a bit more work on calculating steady state entropy production

--- Code/Entp.jl
+++ Code/Entp.jl
@@ -37,23 +37,32 @@ function readin()
     end
     return(vars,times)
 end
+
 # Vector of functions from MAP case
-function f!(G, x, ps)
-    K, k, Q, q, kmin, qmin, f, r, F, Kmin, Qmin = symbols("K k Q q kmin qmin f r F Kmin Qmin")
+function fs1(ps)
+    K, k, Q, q, kmin, qmin, f, r, F, Kmin, Qmin, A, B, S, W = symbols("K k Q q kmin qmin f r F Kmin Qmin A B S W")
     sym = Array{SymEngine.Basic}(4)
-    sym[1] = k*x[3]*r/(r+f*x[2]^2) - K*x[1] - kmin*x[1] + Kmin*x[4]
-    sym[2] = q*x[3]*r/(r+f*x[1]^2) - Q*x[2] - qmin*x[2] + Qmin*x[4]
-    sym[3] = -k*x[3]*r/(r + f*x[2]^2) - q*x[3]*r/(r + f*x[1]^2) + kmin*x[1] + qmin*x[2] + F
-    sym[4] = K*x[1] + Q*x[2] - Kmin*x[4] - Qmin*x[4] - F
+    sym[1] = k*S*r/(r+f*B^2) - K*A - kmin*A + Kmin*W
+    sym[2] = q*S*r/(r+f*A^2) - Q*B - qmin*B + Qmin*W
+    sym[3] = -k*S*r/(r + f*B^2) - q*S*r/(r + f*A^2) + kmin*A + qmin*B + F
+    sym[4] = K*A + Q*B - Kmin*W - Qmin*W - F
     for i = 1:4
         sym[i] = subs(sym[i], K=>ps[1], k=>ps[2], Q=>ps[3], q=>ps[4], kmin=>ps[5], qmin=>ps[6], f=>ps[7])
-        G[i] = subs(sym[i], r=>ps[8], F=>ps[9], Kmin=>ps[10], Qmin=>ps[11]) |> float
+        sym[i] = subs(sym[i], r=>ps[8], F=>ps[9], Kmin=>ps[10], Qmin=>ps[11])
     end
-    return G
+    return(sym)
+end
+
+function f!(f,x,fs)
+    A, B, S, W = symbols("A B S W")
+    for i = 1:4
+        f[i] = subs(fs[i], A=>x[1], B=>x[2], S=>x[3], W=>x[4]) |> float
+    end
+    return(f)
 end
 
 # Diffusion matrix from MAP case
-function D!(D, x, ps)
+function Ds1(ps)
     A, B, S, W, K, k, Q, q, kmin, Kmin, qmin, Qmin, f, r, F = symbols("A B S W K k Q q kmin Kmin qmin Qmin f r F")
     # Make a symbolic version of the matrix, needs no input in this case
     e = Array{SymEngine.Basic,2}(4,4)
@@ -68,55 +77,50 @@ function D!(D, x, ps)
     e[4,4] = sqrt(F) # this is not as previously discussed, could be the error
     # Now do the transformations required
     eT = transpose(e)
-    D = e*eT
-    D = inv(D)
+    Ds = e*eT
+    Ds = inv(Ds)
     for j = 1:4
         for i = 1:4
-           D[i,j] = subs(D[i,j], K=>ps[1], k=>ps[2], Q=>ps[3], q=>ps[4], kmin=>ps[5], qmin=>ps[6], f=>ps[7])
-           D[i,j] = subs(D[i,j], r=>ps[8], F=>ps[9], Kmin=>ps[10], Qmin=>ps[11], A=>x[1], B=>x[2], S=>x[3], W=>x[4]) |> float
+           Ds[i,j] = subs(Ds[i,j], K=>ps[1], k=>ps[2], Q=>ps[3], q=>ps[4], kmin=>ps[5], qmin=>ps[6], f=>ps[7])
+           Ds[i,j] = subs(Ds[i,j], r=>ps[8], F=>ps[9], Kmin=>ps[10], Qmin=>ps[11])
+           Ds[i,j] = expand(Ds[i,j])
        end
     end
-    return D
+    return Ds
+end
+
+function D!(D,x,Ds)
+    A, B, S, W = symbols("A B S W")
+    for j = 1:4
+        for i = 1:4
+            D[i,j] = subs(Ds[i,j], A=>x[1], B=>x[2], S=>x[3], W=>x[4]) |> float
+        end
+    end
+    return(D)
 end
 
 # Function to calculate the action for a given point on the path
-function EntProd(vars::Array{Float64,2},τ::Array{Float64,1},Ω)
-    # parameters hard coded into this function
-    K = 1.0
-    k = 1.0
-    Q = 1.0
-    q = 11/15
-    kmin = 0.5 # now reverse creation is an important process
-    qmin = 0.1
-    f = 1.0 # Promoter switching
-    r = 10.0
-    F = 10.0
-    Kmin = 10.0^-20
-    Qmin = 10.0^-20
-    ps = [ K; k; Q; q; kmin; qmin; f; r; F; Kmin; Qmin]
+function EntProd(vars::Array{Float64,2},τ::Array{Float64,1},Ω::Int64,Ds::Array{SymEngine.Basic,2},ps::Array{Float64,1},
+                fs::Array{SymEngine.Basic,1})
+    h = zeros(4)
+    thiv = zeros(4)
+    d = zeros(4,4)
     termsthi = zeros(4,4)
     termsthif = zeros(4,4)
     termsf = zeros(4,4)
     Fqs = zeros(4)
     Ffs = zeros(4)
-    Fs = 0
-    Acts = 0
     Ents = 0
-    # maybe can avoid the reallocation trap here
-    ############################################################################
-    h = [ 0.0; 0.0; 0.0; 0.0 ]
-    thiv = [ 0.0; 0.0; 0.0; 0.0 ]
-    d = [ 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 ]
-    ############################################################################
     deltat = τ[2] - τ[1]
     posA = (vars[1,1] + vars[1,2])/2
     posB = (vars[2,1] + vars[2,2])/2
     posS = (vars[3,1] + vars[3,2])/2
     posW = (vars[4,1] + vars[4,2])/2
-    h = f!(h, [posA posB posS posW], ps)
+    h = f!(h, [posA posB posS posW], fs)
+    F = ps[9]
     h[3] -= F
     h[4] += F
-    d = D!(d, [posA posB posS posW], ps)
+    d = D!(d, [posA posB posS posW], Ds)
     for j = 1:4
         thiv[j] = (vars[j,2] - vars[j,1])/(deltat)
     end
@@ -145,10 +149,52 @@ function EntProd(vars::Array{Float64,2},τ::Array{Float64,1},Ω)
         Acts += Ffs[k]*deltat
         Ents += Fqs[k]*(2*deltat) # comment this out to see the effect at somepoint
     end
-    return(Acts,Ents,deltat,thiv,Ffs,Fqs,Fs,termsthi,termsf,termsthif)
+    return(Acts,Ents)
+end
+
+# Function to calculate the action for a given point on the path, this time via the Shannon method
+function EntProdShan(vars::Array{Float64,1},τ::Array{Float64,1},ps::Array{Float64,1})
+    # parameters hard coded into this function
+    deltat = τ[2] - τ[1]
+    A = vars[1]
+    B = vars[2]
+    S = vars[3]
+    W = vars[4]
+    K = ps[1]
+    k = ps[2]
+    Q = ps[3]
+    q = ps[4]
+    kmin = ps[5]
+    qmin = ps[6]
+    f = ps[7]
+    r = ps[8]
+    F = ps[9]
+    Kmin = ps[10]
+    Qmin = ps[11]
+    SPA = (k*S*r/(r+f*B^2) - kmin*A)*log((k*S*r/(r+f*B^2))/(kmin*A))*deltat
+    SPB = (q*S*r/(r+f*A^2) - qmin*B)*log((q*S*r/(r+f*A^2))/(kmin*B))*deltat
+    SDA = (K*A - Kmin*W)*log(K*A/(Kmin*W))*deltat
+    SDB = (Q*B - Qmin*W)*log(Q*B/(Qmin*W))*deltat
+    return(SPA,SPB,SDA,SDB)
 end
 
 function main()
+    K = 1.0
+    k = 1.0
+    Q = 1.0
+    q = 11/15
+    kmin = 0.5 # now reverse creation is an important process
+    qmin = 0.1
+    f = 1.0 # Promoter switching
+    r = 10.0
+    F = 10.0
+    Kmin = 10.0^-20
+    Qmin = 10.0^-20
+    Fmin = 10.0^-20
+    ps = [ K; k; Q; q; kmin; qmin; f; r; F; Kmin; Qmin ]
+    # make symbolic arrays
+    Ds = Ds1(ps)
+    fs = fs1(ps)
     # check if argument is provided
     if length(ARGS) >= 1
         vars, times = readin()
@@ -157,23 +203,27 @@ function main()
     end
     # Now need to rescale the trajectories
     Ω = 60
+    N = length(times)
     vars = vars/Ω
     # Now need to do the calculation of entropy production
-    At = St = 0
-    A = zeros(4999999)
-    S = zeros(4999999)
-    for i = 2:5000000#length(times)
-        A[i-1], S[i-1] = EntProd(vars[:,i-1:i],times[i-1:i],Ω)
+    At = St = S1t = S2t = S3t = S4t = S5t = S6t = 0
+    A = zeros(N-1)
+    S = zeros(N-1)
+    for i = 2:N
+        A[i-1], S[i-1] = EntProd(vars[:,i-1:i],times[i-1:i],Ω,Ds,ps,fs)
         At += A[i-1]
         St += S[i-1]
-        if i % 50000 == 0
-            println("$(i),$(At/times[i-1]),$(St/times[i-1])")
+        S1, S2, S3, S4 = EntProdShan(vars[:,i-1],times[i-1:i],ps)
+        S1t += S1
+        S2t += S2
+        S3t += S3
+        S4t += S4
+        if i % 100000 == 0
+            println("$(i),$(At/times[i]),$(St/times[i])")
+            println(((S1t+S2t+S3t+S4t)/times[i]))
         end
     end
-    plot(A)
-    savefig("../Results/A$(ARGS[1]).png")
-    plot(S)
-    savefig("../Results/S$(ARGS[1]).png")
+    
     return(nothing)
 end
 
--- Code/ODEent.jl
+++ Code/ODEent.jl
@@ -5,6 +5,36 @@
 using Roots
 using SymEngine
 
+# ps = [ k, q, kmin, qmin, K, Q, Kmin, Qmin, r, f, F ]
+function p!(p, x, ps)
+    p[1] = ps[1]*ps[9]*x[3]/(ps[9] + ps[10]*x[2]^2) - ps[3]*x[1]
+    p[2] = ps[2]*ps[9]*x[3]/(ps[9] + ps[10]*x[1]^2) - ps[4]*x[2]
+    p[3] = ps[11]
+    p[4] = ps[5]*x[1] - ps[7]*x[4] + ps[6]*x[2] - ps[8]*x[4]
+    return(p)
+end
+
+function d!(d, x, ps)
+    d[1] = ps[5]*x[1] - ps[7]*x[4]
+    d[2] = ps[6]*x[2] - ps[8]*x[4]
+    d[3] = ps[1]*ps[9]*x[3]/(ps[9] + ps[10]*x[2]^2) - ps[3]*x[1] + ps[2]*ps[9]*x[3]/(ps[9] + ps[10]*x[1]^2) - ps[4]*x[2]
+    d[4] = ps[11]
+    return(d)
+end
+
+# Diffusion matrix from MAP case
+function D!(D, x, ps)
+    A, B, S, W, K, k, Q, q, kmin, Kmin, qmin, Qmin, f, r, F = symbols("A B S W K k Q q kmin Kmin qmin Qmin f r F")
+    Dmin = Dmins()
+    for j = 1:4
+        for i = 1:4
+            Dmin[i,j] = subs(Dmin[i,j], A=>x[1], B=>x[2], S=>x[3], W=>x[4], K=>ps[1], k=>ps[2], Q=>ps[3])
+            Dmin[i,j] = subs(Dmin[i,j], q=>ps[4], kmin=>ps[5], Kmin=>ps[6], qmin=>ps[7], Qmin=>ps[8], f=>ps[9], r=>ps[10], F=>ps[11]) |> float
+        end
+    end
+    return Dmin
+end
+
 # make a symbolic diffusion matrix
 function Ds()
     A, B, S, W, K, k, Q, q, kmin, Kmin, qmin, Qmin, f, r, F = symbols("A B S W K k Q q kmin Kmin qmin Qmin f r F")
@@ -95,14 +125,29 @@ function ent2(point::Array{Float64,1},ps::Array{Float64})
     Dmin = Dmins()
     D33 = subs(Dmin[3,3], A=>point[1], B=>point[2], S=>point[3], W=>point[4], K=>ps[1], k=>ps[2], Q=>ps[3])
     D33 = subs(D33, q=>ps[4], kmin=>ps[5], Kmin=>ps[6], qmin=>ps[7], Qmin=>ps[8], f=>ps[9], r=>ps[10], F=>ps[11]) |> float
-    println(D33)
     D44 = subs(Dmin[4,4], A=>point[1], B=>point[2], S=>point[3], W=>point[4], K=>ps[1], k=>ps[2], Q=>ps[3])
     D44 = subs(D44, q=>ps[4], kmin=>ps[5], Kmin=>ps[6], qmin=>ps[7], Qmin=>ps[8], f=>ps[9], r=>ps[10], F=>ps[11]) |> float
-    error()
     S = 2*(ps[11]^2)*(D33 + D44)
     return(S)
 end
 
+function ent3(point::Array{Float64,1},r::Float64,f::Float64,K::Float64,Q::Float64,k::Float64,q::Float64,
+                kmin::Float64,qmin::Float64,Kmin::Float64,Qmin::Float64)
+    pa = k*point[3]*r/(r+f*point[2]^2)
+    pamin = kmin*point[1]
+    pb = q*point[3]*r/(r+f*point[1]^2)
+    pbmin = qmin*point[2]
+    da = K*point[1]
+    damin = Kmin*point[4]
+    db = Q*point[2]
+    dbmin = Qmin*point[4]
+    S1 = (pa - pamin)*log(pa/pamin)
+    S2 = (pb - pbmin)*log(pb/pbmin)
+    S3 = (da - damin)*log(da/damin)
+    S4 = (db - dbmin)*log(db/dbmin)
+    S = S1 + S2 + S3 + S4
+    return(S)
+end
 function main()
     # General parameters
     Ω = 1 # Ω = 1, gMAP parameterisation
@@ -136,6 +181,52 @@ function main()
     println(Sm)
     Sf = ent2(fin,ps)
     println(Sf)
+    p = zeros(4)
+    d = zeros(4)
+    D = zeros(4,4)
+    p = p!(p, star, ps)
+    d = d!(d, star, ps)
+    D = D!(D, star, ps)
+    Ss = 0
+    println(d)
+    println(p)
+    for j = 1:4
+        for i = 1:4
+            Ss += 2*(p[i]*p[j] + d[i]*d[j] - p[i]*d[j] - p[j]*d[i])*D[i,j]
+        end
+    end
+    println(Ss)
+    p = p!(p, mid, ps)
+    d = d!(d, mid, ps)
+    D = D!(D, mid, ps)
+    Sm = 0
+    println(d)
+    println(p)
+    for j = 1:4
+        for i = 1:4
+            Sm += 2*(p[i]*p[j] + d[i]*d[j] - p[i]*d[j] - p[j]*d[i])*D[i,j]
+        end
+    end
+    println(Sm)
+    p = p!(p, fin, ps)
+    d = d!(d, fin, ps)
+    D = D!(D, fin, ps)
+    println(d)
+    println(p)
+    Sf = 0
+    for j = 1:4
+        for i = 1:4
+            Sf += 2*(p[i]*p[j] + d[i]*d[j] - p[i]*d[j] - p[j]*d[i])*D[i,j]
+        end
+    end
+    println(Sf)
+    Ss = ent3(star,r,f,K,Q,k,q,kmin,qmin,Kmin,Qmin)
+    println(Ss)
+    Sm = ent3(mid,r,f,K,Q,k,q,kmin,qmin,Kmin,Qmin)
+    println(Sm)
+    Sf = ent3(fin,r,f,K,Q,k,q,kmin,qmin,Kmin,Qmin)
+    println(Sf)
+
 end
 
 @time main()

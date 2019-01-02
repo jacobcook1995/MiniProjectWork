693cbe62ec8182e36032944d70ade19134d71a11
jacobcookmac <jc2017@ic.ac.uk>
2019-01-02 11:32:38 +0000

    Got plotting running nicely

--- Code/PaperCode2/gill2traj.jl
+++ Code/PaperCode2/gill2traj.jl
@@ -8,22 +8,54 @@
 
 using Roots
 using Plots
-import GR
+using LaTeXStrings
+using PyCall
+pygui(:qt5)
+import PyPlot
 
 # function to find start end and saddle points
-function nullcline(r::Float64,f::Float64,K::Float64,Q::Float64,k::Float64,q::Float64,kmin::Float64,qmin::Float64)
+function nullcline(r::Float64,f::Float64,K::Float64,Q::Float64,k::Float64,q::Float64,kmin::Float64,qmin::Float64,Kmin::Float64,Qmin::Float64)
     a = 2
     b = 2
-    A1(x) = k*r/(K*(r+f*x^a))
-    A2(x) = (r/f*(q/(Q*x)-1))^(1/b)
-    g(x) = k*r/(K*(r+f*x^a)) - (r/f*(q/(Q*x)-1))^(1/b) #A1(x) - A2(x)
-    xs = fzeros(g, 0, q/Q)
-    ss1 = [A1(xs[1]); xs[1]]
-    sad = [A1(xs[2]); xs[2]]
-    ss2 = [A1(xs[3]); xs[3]]
+    A1(x) = real(sqrt(complex((r/f)*(q/((qmin+Q)*x - Qmin) - 1))))
+    A2(x) = (1/(kmin+K))*((k*r)/(r + f*x^2) + Kmin)
+    g(x) = A1(x) - A2(x)
+    three = false
+    n = 0
+    bs = []
+    # then setup loop to find three solutions to these equations
+    while three == false
+        bs1 = fzeros(g, 0.0, 0.1) # catches zeros about the origin
+        bs2 = fzeros(g, 0.1, 2*q/Q) # the upper bound here is potentially problematic
+        bs = vcat(bs1,bs2)
+        n = length(bs)
+        gs = 0
+        bad = zeros(Int64,0)
+        for i = 1:n
+            # check if this is actual solution and not artifact of the numerical method
+            gs = g(bs[i])
+            tol = 1.0e-14
+            if gs >= 0 + tol || gs <= 0 - tol
+                bad = append!(bad,i)
+            end
+        end
+        if length(bad) != 0
+            n = n - length(bad)
+            bs = deleteat!(bs, bad)
+        end
+        if n == 3
+            three = true
+        end
+    end
+    # now make the stationary points
+    ss1 = [ A1(bs[1]), bs[1] ]
+    sad = [ A1(bs[2]), bs[2] ]
+    ss2 = [ A1(bs[3]), bs[3] ]
+    println("The three stationary points are:")
     println(ss1)
     println(sad)
     println(ss2)
+    flush(stdout) # needed to ensure output prints when using nohup
     return(ss1,sad,ss2)
 end
 
@@ -38,13 +70,6 @@ function rates(A::Int64,B::Int64,k::Float64,K::Float64,q::Float64,Q::Float64,kmi
         dB = [ 0, 0, 0, 0, 1, -1, -1, 1]
         return(rates,dA,dB)
     end
-end
-
-# function to calculate the time step
-function timstep(rates::Array{Float64,1})
-    r = rand()
-    τ = -log(r)/sum(rates)
-    return(τ)
 end
 
 # function to advance gillespie one step
@@ -73,108 +98,66 @@ end
 
 # function to actually run a gillepsie simulation
 function gillespie(K::Float64,k::Float64,Q::Float64,q::Float64,kmin::Float64,qmin::Float64,
-                    f::Float64,r::Float64,Kmin::Float64,Qmin::Float64,noits::Int64,star::Array{Int64,1},Ω::Int64)
+                    f::Float64,r::Float64,Kmin::Float64,Qmin::Float64,star::Array{Int64,1},
+                    fin::Array{Int64,1},Ω::Int64)
     # change this so that it uses less memory
-    ts = zeros(noits+1)
-    ABs = zeros(noits+1,2)
-    t = 0
-    AB = copy(star)
-    ts[1] = t
-    ABs[1,:] = AB
-    for i = 1:noits
-        # calculate rates
-        rs = rates(AB[1],AB[2],k,K,q,Q,kmin,Kmin,qmin,Qmin,r,f)
-        # calculate timestep
-        τ = timstep(rs)
-        # update time
-        t += τ
-        # do gillepsie step
-        AB = step(rs,AB)
-        # add to vectors
-        ts[i+1] = t
-        ABs[i+1,:] = AB
-    end
+    ABf = zeros(1001,2)
+    ABb = zeros(1001,2)
+    forw = false
+    back = false
+    ABf0 = copy(star)
+    ABf[1,:] = ABf0
+    ABb0 = copy(fin)
+    ABb[1,:] = ABb0
+    # for i = 1:noits
+    #     # calculate rates
+    #     rs = rates(AB[1],AB[2],k,K,q,Q,kmin,Kmin,qmin,Qmin,r,f)
+    #     # do gillepsie step
+    #     AB = step(rs,AB)
+    #     # add to vectors
+    #     ABs[i+1,:] = AB
+    # end
     println("Gillespie Done!")
-    return(ABs,ts)
+    return(ABf,ABb)
 end
 
 # main function
 function main()
     # General parameters
     Ω = 150
-    K = 10.0
-    k = K*Ω # steady state for A=k/K=1
-    Q = 1.0
-    q = Q*Ω
-    kmin = 10.0^-20 # set all too 10.0^-20 for now
-    Kmin = 10.0^-20
-    qmin = 10.0^-20
-    Qmin = 10.0^-20
-    f = 1000.0/(Ω^2) # Promoter switching
-    r = 100.0
-
+    k = 26.097758831774257
+    kmin = 2.179419011537317
+    q = 25.772181639137496
+    qmin = 1.4148644973536413
+    K = 8.24009582574649
+    Kmin = 0.6004846952009286
+    Q = 6.96376978178525
+    Qmin = 0.1657402116759403
+    r = 3875.552664673383
+    f = 9285.774344574023
     # first need to use these parameters to find a steady state
-    star1, mid1, fin1 = nullcline(r,f,K,Q,k,q,kmin,qmin)
+    star1, mid1, fin1 = nullcline(r,f,K,Q,k,q,kmin,qmin,Kmin,Qmin)
     # round star so that it becomes a vector of integers
     star2 = fill(0,2)
     mid2 = fill(0,2)
     fin2 = fill(0,2)
     for i = 1:2
-        star2[i] = round(Int64,star1[i])
-        mid2[i] = round(Int64,mid1[i])
-        fin2[i] = round(Int64,fin1[i])
+        star2[i] = round(Int64,star1[i]*Ω)
+        mid2[i] = round(Int64,mid1[i]*Ω)
+        fin2[i] = round(Int64,fin1[i]*Ω)
     end
     # now run gillespie
-    noits = 250000000
-    # maybe randomise the starting point somewhat
-    # also maybe remove the vars
-    ABs, ts = gillespie(K,k,Q,q,kmin,qmin,f,r,Kmin,Qmin,noits,fin2,Ω)
-    # should actually find the crossing points then just plot
-    trig = false
-    pos1 = 0
-    while trig == false
-        pos1 += 1
-        if ABs[pos1,1] >= star2[1] && ABs[pos1,2] <= star2[2]
-            trig = true
-        elseif pos1 == noits
-            error("Not long enough simulation\n")
-        end
-    end
-    intv = 50000
-    ABsr = ABs[(pos1-intv):(pos1),:]
-    tsr = ts[(pos1-intv):(pos1)]
-    trig = false
-    pos2 = 0
-    while trig == false
-        pos2 += 1
-        if ABsr[intv-pos2,2] >= fin2[2] && ABsr[intv-pos2,1] <= fin2[1]
-            trig = true
-        end
-    end
-    trig = false
-    pos3 = pos1
-    while trig == false
-        pos3 += 1
-        if ABs[pos3,2] >= fin2[2] && ABs[pos3,1] <= fin2[1]
-            trig = true
-        elseif pos3 == noits
-            error("Not long enough simulation\n")
-        end
-    end
-    ABsf = ABs[(pos3-intv):(pos3),:]
-    tsf = ts[(pos3-intv):(pos3)]
-    trig = false
-    pos4 = 0
-    while trig == false
-        pos4 += 1
-        if ABsf[intv-pos4,2] <= star2[2] && ABsf[intv-pos4,1] >= star2[1]
-            trig = true
-        end
-    end
-    plot(ABsr[(intv-pos2):end,2],ABsr[(intv-pos2):end,1],label="to high A",title="2D Toggle Switch")
-    plot!(ABsf[(intv-pos4):end,2],ABsf[(intv-pos4):end,1],label="to high B",xlabel="A",ylabel="B")
-    scatter!([star2[2]], [star2[1]], seriescolor = :green, label="")
-    scatter!([fin2[2]], [fin2[1]], seriescolor = :red, label="")
+    ABf, ABb = gillespie(K,k,Q,q,kmin,qmin,f,r,Kmin,Qmin,star2,fin2,Ω)
+    # switch to PyPlot
+    pyplot()
+    # DEfine relevant latex strings
+    LatS = latexstring("\\Omega = $(Ω)")
+    # Then plot graphs
+    plot(ABf[:,2]/Ω,ABf[:,1]/Ω,label="to high A",title="2D Toggle Switch",legendtitle=LatS)
+    plot!(ABb[:,2]/Ω,ABb[:,1]/Ω,label="to high B",xlabel="A",ylabel="B")
+    scatter!([star2[2]/Ω], [star2[1]/Ω], seriescolor = :green, label="")
+    scatter!([mid2[2]/Ω], [mid2[1]/Ω], seriescolor = :orange, label="")
+    scatter!([fin2[2]/Ω], [fin2[1]/Ω], seriescolor = :red, label="")
     savefig("../Results/switch.png")
     return(nothing)
 end

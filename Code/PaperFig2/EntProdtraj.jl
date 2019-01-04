3fc0ceaae53ec70968cea4ccdbbd50824d663919
jacobcookmac <jc2017@ic.ac.uk>
2019-01-04 15:24:13 +0000

    Got new entropy production calculation method working

--- Code/PaperFig2/EntProdtraj.jl
+++ Code/PaperFig2/EntProdtraj.jl
@@ -53,9 +53,185 @@ function LangEnt(traj::Array{Float64,2},ps::Array{Float64,1},dt::Float64)
     return(entp)
 end
 
+# function that takes in two points and finds the probability that the switch that generates them happens
+function probs(star::Array{Int64,1},fin::Array{Int64,1},k::Float64,K::Float64,q::Float64,Q::Float64,
+                kmin::Float64,Kmin::Float64,qmin::Float64,Qmin::Float64,r::Float64,f::Float64)
+    rates = [ k*r/(r+f*star[2]^2), kmin*star[1], K*star[1], Kmin, q*r/(r+f*star[1]^2), qmin*star[2], Q*star[2], Qmin ]
+    # not actually possible to distinguish two processes, which will effect the probabilities
+    if fin[1] - star[1] == 0
+        if fin[2] - star[2] == 1
+            P = (rates[5] + rates[8])/sum(rates)
+        elseif fin[2] - star[2] == -1
+            P = (rates[6] + rates[7])/sum(rates)
+        else
+            error()
+        end
+    else
+        if fin[1] - star[1] == 1
+            P = (rates[1] + rates[4])/sum(rates)
+        elseif fin[1] - star[1] == -1
+            P = (rates[2] + rates[3])/sum(rates)
+        else
+            error()
+        end
+    end
+    return(P)
+end
+
 # A function to find and plot the reduced master equation entropy productions of the various trajectories
-function LangEnt(traj::Array{Float64,2},ps::Array{Float64,1},dt::Float64)
-    
+function MastEnt(traj::Array{Float64,2},ps::Array{Float64,1},T::Float64,Ω::Int64,red::Int64)
+    k = ps[1]
+    kmin = ps[2]
+    q = ps[3]
+    qmin = ps[4]
+    K = ps[5]
+    Kmin = ps[6]
+    Q = ps[7]
+    Qmin = ps[8]
+    r = ps[9]
+    f = ps[10]
+    # rescale rates appropriately
+    k = k*Ω
+    Kmin = Kmin*Ω
+    q = q*Ω
+    Qmin = Qmin*Ω
+    f = f/(Ω^2)
+    # rescale trajectory
+    traj = traj*Ω
+    # Should now make path in A,B
+    path = Array{Int64,2}(undef,0,2)
+    ts = Array{Float64,1}(undef,0)
+    # approximate steady state
+    val = fill(0,1,2)
+    for i = 1:2
+        val[i] = round(Int64,traj[1,i])
+    end
+    # initial time t=0
+    t = 0
+    ts = vcat(ts,t)
+    # and add as start of path
+    path = vcat(path,val)
+    L = size(traj,1)
+    # want to find point where A/B crossed the 0.5 point
+    for i = 2:L
+        # first establish if there has been any change from the prior step
+        if round(Int64,traj[i,1]) == path[end,1] && round(Int64,traj[i,2]) == path[end,2]
+            # if no change between successive points no reason to update path
+        else
+            # count changes to A and B between two points
+            dA = round(Int64,traj[i,1]) - path[end,1]
+            dB = round(Int64,traj[i,2]) - path[end,2]
+            th = (i-1)*T/(L-1) # Time here
+            tp = (i-2)*T/(L-1) # Time at prior step
+            vals = Array{Int64,2}(undef,0,2) # array to store values
+            tempt = Array{Float64,1}(undef,0) # vector to temporaily store times
+            if dA != 0
+                # find difference in A in interval
+                da = (traj[i,1] - traj[i-1,1])
+                # find first & potentially only point of change
+                ap = round(Int64,traj[i,1]) - 0.5*sign(dA)
+                # find distance from point of point of change
+                deltaA = traj[i,1] - ap
+                # as a fraction of spacing
+                fracA = deltaA/da
+                # find time and save
+                t = th - fracA*(th - tp)
+                tempt = vcat(tempt,t)
+                # find B and save point
+                B = traj[i,2] - fracA*(traj[i,2] - traj[i-1,2])
+                val[1] = round(Int64,traj[i,1])
+                val[2] = round(Int64,B)
+                vals = vcat(vals,val)
+                n = 1 # first point done
+                # loop until all points are done
+                done = false
+                while done == false
+                    if n == abs(dA)
+                        done = true
+                        break
+                    end
+                    deltaA = 1*sign(dA)
+                    fracA = deltaA/da
+                    # remove time of gap from time
+                    tn = fracA*(th - tp)
+                    t -= tn
+                    tempt = vcat(tempt,t)
+                    B -= fracA*(traj[i,2] - traj[i-1,2])
+                    val[1] = round(Int64,traj[i,1]) - n*sign(dA)
+                    val[2] = round(Int64,B)
+                    vals = vcat(vals,val)
+                    n += 1 # another point completed
+                end
+            end
+            if dB != 0
+                # find difference in B in interval
+                db = (traj[i,2] - traj[i-1,2])
+                # find first & potentially only point of change
+                bp = round(Int64,traj[i,2]) - 0.5*sign(dB)
+                # find distance from point of point of change
+                deltaB = traj[i,2] - bp
+                # as a fraction of spacing
+                fracB = deltaB/db
+                # find time and save
+                t = th - fracB*(th - tp)
+                tempt = vcat(tempt,t)
+                # find A and save point
+                A = traj[i,1] - fracB*(traj[i,1] - traj[i-1,1])
+                val[1] = round(Int64,A)
+                val[2] = round(Int64,traj[i,2])
+                vals = vcat(vals,val)
+                n = 1 # first point done
+                # loop until all points are done
+                done = false
+                while done == false
+                    if n == abs(dB)
+                        done = true
+                        break
+                    end
+                    deltaB = 1*sign(dB)
+                    fracB = deltaB/db
+                    # remove time of gap from time
+                    tn = fracB*(th - tp)
+                    t -= tn
+                    tempt = vcat(tempt,t)
+                    A -= fracB*(traj[i,1] - traj[i-1,1])
+                    val[1] = round(Int64,A)
+                    val[2] = round(Int64,traj[i,2]) - n*sign(dB)
+                    vals = vcat(vals,val)
+                    n += 1 # another point completed
+                end
+            end
+            # Now reorder vals by the times
+            p = sortperm(tempt)
+            tempt = sort(tempt)
+            vals = vals[p,:]
+            # then vcat to path
+            ts = vcat(ts,tempt)
+            path = vcat(path,vals)
+        end
+    end
+    # need both forward and backward trajectories in order to establish entropy production
+    backp = path[end:-1:1,:]
+    backts = ts[end:-1:1]
+    len = length(ts)-1
+    Pf = zeros(len)
+    Pb = zeros(len)
+    for i = 2:len+1
+        Pf[i-1] = probs(path[i-1,:],path[i,:],k,K,q,Q,kmin,Kmin,qmin,Qmin,r,f)
+        Pb[i-1] = probs(backp[i-1,:],backp[i,:],k,K,q,Q,kmin,Kmin,qmin,Qmin,r,f)
+    end
+    entp = log.(Pf./Pb[end:-1:1])
+    len2 = floor(Int64,len/red)+1
+    entsr = zeros(len2)
+    As = zeros(len2)
+    for i = 1:len2-1
+        As[i] = sum(path[(1+(red*(i-1))):(red*i),1])/red
+        entsr[i] = sum(entp[(1+(red*(i-1))):(red*i)])
+    end
+    entsr[end] = sum(entp[(red*(len2-1)+1):end])
+    As[end] = sum(path[(red*(len2-1)+1):end,1])/length(path[(red*(len2-1)+1):end,1])
+    # finally extract the path in A
+    entp = entsr
     return(entp,As)
 end
 function main()
@@ -196,24 +372,32 @@ function main()
     # Now calculate and plot langevin entropy productions
     LatS = L"\Delta S_{L}"
     pyplot()
+    # for i = 1:len
+    #     for j = 1:2
+    #         d = 2*(j-1)+4*(i-1)
+    #         entp = LangEnt(traj[:,(1+d):(2+d)],ps[i,:],Act[2*(i-1)+j,1]/N)
+    #         plot(traj[1:end-1,1+d],entp,label=LatS,dpi=300,legend=:best)
+    #         plot!(xlabel="A",ylabel="Ent Prod")
+    #         scatter!([steads[i,1+4*(j-1)]],[0.0],color=:green,label="Start")
+    #         scatter!([steads[i,3]],[0.0],color=:yellow,label="Saddle")
+    #         scatter!([steads[i,5-4*(j-1)]],[0.0],color=:red,label="End")
+    #         savefig("../Results/Fig2Graphs/$(i)$(j)LangEnt.png")
+    #     end
+    # end
+    LatS2 = L"\Delta S_{M}"
+    Ω = 5000
+    LatS3 = latexstring("\\Omega = $(Ω)")
+    red = 250 # batch size
     for i = 1:len
         for j = 1:2
             d = 2*(j-1)+4*(i-1)
-            entp = LangEnt(traj[:,(1+d):(2+d)],ps[i,:],Act[2*(i-1)+j,1]/N)
-            plot(traj[1:end-1,1+d],entp,label=LatS,dpi=300,legend=:best)
-            plot!(xlabel="A",ylabel="Ent Prod")
+            entp2, As = MastEnt(traj[:,(1+d):(2+d)],ps[i,:],Act[2*(i-1)+j,1],Ω,red)
+            plot(As/Ω,entp2/Ω,label=LatS2,dpi=300,legend=:best)
+            plot!(xlabel="A",ylabel="Ent Prod",legendtitle=LatS3)
             scatter!([steads[i,1+4*(j-1)]],[0.0],color=:green,label="Start")
             scatter!([steads[i,3]],[0.0],color=:yellow,label="Saddle")
             scatter!([steads[i,5-4*(j-1)]],[0.0],color=:red,label="End")
-            savefig("../Results/Fig2Graphs/$(i)$(j)LangEnt.png")
-        end
-    end
-    LatS2 = L"\\Delta S_{M}"
-    Ω = 5000
-    for i = 1:len
-        for j = 1:2
-            d = 2*(j-1)+4*(i-1)
-            entp2, As = MastEnt(traj[:,(1+d):(2+d)],ps[i,:],Act[2*(i-1)+j,1]/N)
+            savefig("../Results/Fig2Graphs/$(i)$(j)MastEnt.png")
         end
     end
     return(nothing)

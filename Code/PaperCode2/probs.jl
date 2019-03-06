29e6c9c029f1dd10f87befb43823bd38dc058909
Jacob Cook <jc2017@ic.ac.uk>
2019-03-06 16:45:03 +0000

    some more updates to comments

--- Code/PaperCode2/probs.jl
+++ Code/PaperCode2/probs.jl
@@ -229,7 +229,7 @@ function main()
     # inds = [15,16,17,18,20,21,58,85] # reached 18 # 3 of 8 done
     # inds = [52,53,55,56,57] # reached 57 # 4 of 5 done
     # inds = [46,50,51] # reached 51 # 2 of 3 done
-    # inds = [36,38,40] # reached 36 # 0 of 3 done
+    # inds = [36,38,40] # reached 40 # 2 of 3 done
     # inds = [42,31,44,32] # indicies that do not work
     # inds = [43] # too fast
     inds = 1:100 # Do not run with this as it would take weeks

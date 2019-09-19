8320ba3d18459791a02c53d0b02b29ea46ee4d99
Jacob Cook <jc2017@ic.ac.uk>
2019-09-19 12:54:28 +0100

    Started writing function to make a heatmap

--- Code/PaperCode2/combplots.jl
+++ Code/PaperCode2/combplots.jl
@@ -1205,6 +1205,78 @@ function fifth()
     scatter!([entS/1000],[entG/1000],label="Toggle switch",color=1,legend=:topleft)
     plot!(xlabel="Ent prod $(pers) (Schnakenberg)",ylabel="Ent prod $(pers) (Master Eq)")
     savefig("../Results/SchnakvsGill.png")
+    return(nothing)
 end
 
-@time fifth()
+# function to make a heat map of conservative actiosn showing that only small area is feasible
+function sixth()
+    println("Compiled, Starting script.")
+    # First read in parameter sets
+    infile = "../Results/Fig3Data/$(ARGS[1])para.csv"
+    if ~isfile(infile)
+        println("Error: No file of parameters to be read.")
+        return(nothing)
+    end
+    # now read in parameters
+    l = countlines(infile)
+    w = 10
+    ps = zeros(l,w)
+    open(infile, "r") do in_file
+        # Use a for loop to process the rows in the input file one-by-one
+        k = 1
+        for line in eachline(in_file)
+            # parse line by finding commas
+            L = length(line)
+            comma = fill(0,w+1)
+            j = 1
+            for i = 1:L
+                if line[i] == ','
+                    j += 1
+                    comma[j] = i
+                end
+            end
+            comma[end] = L+1
+            for i = 1:w
+                ps[k,i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+            end
+            k += 1
+        end
+    end
+    # Check file of steady states exists
+    infile = "../Results/Fig3Data/$(ARGS[1])stead.csv"
+    if ~isfile(infile)
+        println("Error: No file of parameters to be read.")
+        return(nothing)
+    end
+    # now read in steady states
+    l = countlines(infile)
+    w = 6
+    steads = zeros(l,w)
+    open(infile, "r") do in_file
+        # Use a for loop to process the rows in the input file one-by-one
+        k = 1
+        for line in eachline(in_file)
+            # parse line by finding commas
+            L = length(line)
+            comma = fill(0,w+1)
+            j = 1
+            for i = 1:L
+                if line[i] == ','
+                    j += 1
+                    comma[j] = i
+                end
+            end
+            comma[end] = L+1
+            for i = 1:w
+                steads[k,i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+            end
+            k += 1
+        end
+    end
+    N = 100 # Number that I have chosen to use
+    # Now want to make heat map of conservative actions and probably overlay actual switching paths
+    # Print out parameters used for good measure
+    return(nothing)
+end
+
+@time sixth()

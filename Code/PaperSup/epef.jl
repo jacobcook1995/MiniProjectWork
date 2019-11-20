97af5be4e548804b9caff936442f8f5e969c762b
Jacob Cook <jc2017@ic.ac.uk>
2019-11-20 16:46:49 +0000

    Done for day

--- Code/PaperSup/epef.jl
+++ Code/PaperSup/epef.jl
@@ -8,6 +8,7 @@
 
 using Plots
 using LaTeXStrings
+using Statistics
 import PyPlot
 
 # One plotting function in this script that should hopefully obtain everything needed
@@ -191,71 +192,255 @@ function second()
         println("Error: Need to provide a name for Schlögl inputs.")
         return(nothing)
     end
-    # Need to calculate both EP and EF terms along path
-    # Therefore need to read in parameter sets
-    # First read in parameter sets
-    infile = "../Results/Fig3Data/$(ARGS[1])para.csv"
-    if ~isfile(infile)
-        println("Error: No file of parameters to be read for toggle.")
-        return(nothing)
-    end
-    # now read in parameters
-    l = countlines(infile)
-    w = 10
-    ps = zeros(l,w)
-    open(infile, "r") do in_file
-        # Use a for loop to process the rows in the input file one-by-one
-        k = 1
-        for line in eachline(in_file)
-            # parse line by finding commas
-            L = length(line)
-            comma = fill(0,w+1)
-            j = 1
-            for i = 1:L
-                if line[i] == ','
-                    j += 1
-                    comma[j] = i
+    # Can find the EP and EF values from a file
+    # Make a structure to store the EP and EF terms for the toggle switch model
+    l = 100
+    ef = zeros(l,8)
+    datayn = fill(true,l)
+    for i = 1:l
+        for j = 1:2
+            # set file to read in
+            if j == 1
+                infile = "../Results/Fig3Data/Traj/$(i)$(ARGS[1])A2Bpf.csv"
+            else
+                infile = "../Results/Fig3Data/Traj/$(i)$(ARGS[1])B2Apf.csv"
+            end
+            # check it exits
+            if isfile(infile)
+                w = 4
+                open(infile, "r") do in_file
+                    # only one line now
+                    for line in eachline(in_file)
+                        # parse line by finding commas
+                        L = length(line)
+                        comma = fill(0,w+1)
+                        m = 1
+                        for k = 1:L
+                            if line[k] == ','
+                                m += 1
+                                comma[m] = k
+                            end
+                        end
+                        comma[end] = L+1
+                        for k = 1:w
+                            ef[i,(j-1)*4+k] = parse(Float64,line[(comma[k]+1):(comma[k+1]-1)])
+                        end
+                    end
+                end
+            else # if file doesn't exist inform user of missing data and exclude from plots
+                if j == 1
+                    println("Missing data for run $(i) high A to high B")
+                    datayn[i] = false
+                else
+                    println("Missing data for run $(i) high B to high A")
+                    datayn[i] = false
                 end
             end
-            comma[end] = L+1
-            for i = 1:w
-                ps[k,i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
-            end
-            k += 1
         end
     end
-    # The same for the Schlögl model
-    infile = "../Results/Fig3DataS/$(ARGS[2])paraS.csv"
-    if ~isfile(infile)
-        println("Error: No file of parameters to be read for Schlögl.")
-        return(nothing)
-    end
-    # now read in parameters
-    l = countlines(infile)
-    w = 4
-    psS = zeros(l,w)
-    open(infile, "r") do in_file
-        # Use a for loop to process the rows in the input file one-by-one
-        k = 1
-        for line in eachline(in_file)
-            # parse line by finding commas
-            L = length(line)
-            comma = fill(0,w+1)
-            j = 1
-            for i = 1:L
-                if line[i] == ','
-                    j += 1
-                    comma[j] = i
+    # Make a structure to store the EP and EF terms for the Schlögl model
+    Sef = zeros(l,8)
+    Sdatayn = fill(true,l)
+    for i = 1:l
+        for j = 1:2
+            # set file to read in
+            if j == 1
+                infile = "../Results/Fig3DataS/Traj/$(i)$(ARGS[2])h2lpf.csv"
+            else
+                infile = "../Results/Fig3DataS/Traj/$(i)$(ARGS[2])l2hpf.csv"
+            end
+            # check it exits
+            if isfile(infile)
+                w = 4
+                open(infile, "r") do in_file
+                    # only one line now
+                    for line in eachline(in_file)
+                        # parse line by finding commas
+                        L = length(line)
+                        comma = fill(0,w+1)
+                        m = 1
+                        for k = 1:L
+                            if line[k] == ','
+                                m += 1
+                                comma[m] = k
+                            end
+                        end
+                        comma[end] = L+1
+                        for k = 1:w
+                            Sef[i,(j-1)*4+k] = parse(Float64,line[(comma[k]+1):(comma[k+1]-1)])
+                        end
+                    end
+                end
+            else # if file doesn't exist inform user of missing data and exclude from plots
+                if j == 1
+                    println("Missing data for run $(i) high A to high B")
+                    Sdatayn[i] = false
+                else
+                    println("Missing data for run $(i) high B to high A")
+                    Sdatayn[i] = false
                 end
             end
-            comma[end] = L+1
-            for i = 1:w
-                psS[k,i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
-            end
-            k += 1
         end
     end
-    # Now need to read in trajectories
+    # Now need to obtain the master equation switching data
+    means = zeros(l,4)
+    for i = 1:l
+        infile1 = "../Results/Fig3Data/Traj/$(i)$(ARGS[1])A2BMast.csv"
+        infile2 = "../Results/Fig3Data/Traj/$(i)$(ARGS[1])B2AMast.csv"
+        # Extract forward data
+        ne = 101
+        w = ne
+        mastf = zeros(w-1)
+        open(infile1, "r") do in_file
+            # Use a for loop to process the rows in the input file one-by-one
+            k = 1
+            for line in eachline(in_file)
+                # parse line by finding commas
+                L = length(line)
+                comma = fill(0,w+1)
+                j = 1
+                for i = 1:L
+                    if line[i] == ','
+                        j += 1
+                        comma[j] = i
+                    end
+                end
+                comma[end] = L+1
+                for i = 1:w-1
+                    mastf[i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+                end
+                k += 1
+            end
+        end
+        # Then extract backwards data
+        mastb = zeros(w-1)
+        open(infile2, "r") do in_file
+            # Use a for loop to process the rows in the input file one-by-one
+            k = 1
+            for line in eachline(in_file)
+                # parse line by finding commas
+                L = length(line)
+                comma = fill(0,w+1)
+                j = 1
+                for i = 1:L
+                    if line[i] == ','
+                        j += 1
+                        comma[j] = i
+                    end
+                end
+                comma[end] = L+1
+                for i = 1:w-1
+                    mastb[i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+                end
+                k += 1
+            end
+        end
+        # Now need to extract averages and standard deviations and plot against other data
+        means[i,1] = mean(mastf)
+        means[i,2] = mean(mastb)
+        means[i,3] = stdm(mastf,means[i,1];corrected=true)/sqrt(ne-1)
+        means[i,4] = stdm(mastb,means[i,2];corrected=true)/sqrt(ne-1)
+    end
+    # Now repeat entire process for the Schlögl model
+    Smeans = zeros(l,4)
+    for i = 1:l
+        infile1 = "../Results/Fig3DataS/Traj/$(i)$(ARGS[2])h2lMast.csv"
+        infile2 = "../Results/Fig3DataS/Traj/$(i)$(ARGS[2])l2hMast.csv"
+        # Extract forward data
+        ne = 101
+        w = ne
+        mastf = zeros(w-1)
+        open(infile1, "r") do in_file
+            # Use a for loop to process the rows in the input file one-by-one
+            k = 1
+            for line in eachline(in_file)
+                # parse line by finding commas
+                L = length(line)
+                comma = fill(0,w+1)
+                j = 1
+                for i = 1:L
+                    if line[i] == ','
+                        j += 1
+                        comma[j] = i
+                    end
+                end
+                comma[end] = L+1
+                for i = 1:w-1
+                    mastf[i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+                end
+                k += 1
+            end
+        end
+        # Then extract backwards data
+        mastb = zeros(w-1)
+        open(infile2, "r") do in_file
+            # Use a for loop to process the rows in the input file one-by-one
+            k = 1
+            for line in eachline(in_file)
+                # parse line by finding commas
+                L = length(line)
+                comma = fill(0,w+1)
+                j = 1
+                for i = 1:L
+                    if line[i] == ','
+                        j += 1
+                        comma[j] = i
+                    end
+                end
+                comma[end] = L+1
+                for i = 1:w-1
+                    mastb[i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+                end
+                k += 1
+            end
+        end
+        # Now need to extract averages and standard deviations and plot against other data
+        Smeans[i,1] = mean(mastf)
+        Smeans[i,2] = mean(mastb)
+        Smeans[i,3] = stdm(mastf,means[i,1];corrected=true)/sqrt(ne-1)
+        Smeans[i,4] = stdm(mastb,means[i,2];corrected=true)/sqrt(ne-1)
+    end
+    # First compare EP terms with master equation entropy productions
+    pyplot(dpi=200)
+    plot(xlabel="EP term",ylabel="Exact (Gillespie)",title="Path entropy productions toggle")
+    # First plot one set
+    for i = 1:l
+        if i == 1
+            plot!([ef[i,1]],[means[i,1]],yerror=[means[i,3]],label=L"\Delta S_{A{\rightarrow}B}",color=1)
+        else
+            plot!([ef[i,1]],[means[i,1]],yerror=[means[i,3]],label="",color=1)
+        end
+    end
+    # and then the other
+    for i = 1:l
+        if i == 1
+            plot!([ef[i,5]],[means[i,2]],yerror=[means[i,4]],label=L"\Delta S_{B{\rightarrow}A}",color=2)
+        else
+            plot!([ef[i,5]],[means[i,2]],yerror=[means[i,4]],label="",color=2)
+        end
+    end
+    savefig("../Results/EPEF/switchT.png")
+    plot(xlabel="EP term",ylabel="Exact (Gillespie)",title="Path entropy productions Schlögl")
+    # First plot one set
+    for i = 1:l
+        if i == 1
+            plot!([Sef[i,1]],[Smeans[i,1]],yerror=[Smeans[i,3]],label=L"\Delta S_{A{\rightarrow}B}",color=1)
+        else
+            plot!([Sef[i,1]],[Smeans[i,1]],yerror=[Smeans[i,3]],label="",color=1)
+        end
+    end
+    # and then the other
+    for i = 1:l
+        if i == 1
+            plot!([Sef[i,5]],[Smeans[i,2]],yerror=[Smeans[i,4]],label=L"\Delta S_{B{\rightarrow}A}",color=2)
+        else
+            plot!([Sef[i,5]],[Smeans[i,2]],yerror=[Smeans[i,4]],label="",color=2)
+        end
+    end
+    savefig("../Results/EPEF/switchS.png")
+    # Do differences
+    # Then same with entropy flow
+    # Also need to compare to Langevin entropy production => As Robert is sure to ask about this
     return(nothing)
 end
 

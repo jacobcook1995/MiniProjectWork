023ce95f09e07fd7a1f764df1e1989786b582fb8
Jacob Cook <jc2017@ic.ac.uk>
2019-11-20 13:53:53 +0000

    Started second plotting function

--- Code/PaperSup/epef.jl
+++ Code/PaperSup/epef.jl
@@ -182,6 +182,80 @@ end
 
 # A second function to find production and flow terms along the paths
 function second()
+    println("Compiled, Starting script.")
+    # First check that an argument for naming has been provided
+    if length(ARGS) == 0
+        println("Error: Need to provide a name for toggle switch inputs")
+        return(nothing)
+    elseif length(ARGS) == 1
+        println("Error: Need to provide a name for Schlögl inputs.")
+        return(nothing)
+    end
+    # Need to calculate both EP and EF terms along path
+    # Therefore need to read in parameter sets
+    # First read in parameter sets
+    infile = "../Results/Fig3Data/$(ARGS[1])para.csv"
+    if ~isfile(infile)
+        println("Error: No file of parameters to be read for toggle.")
+        return(nothing)
+    end
+    # now read in parameters
+    l = countlines(infile)
+    w = 10
+    ps = zeros(l,w)
+    open(infile, "r") do in_file
+        # Use a for loop to process the rows in the input file one-by-one
+        k = 1
+        for line in eachline(in_file)
+            # parse line by finding commas
+            L = length(line)
+            comma = fill(0,w+1)
+            j = 1
+            for i = 1:L
+                if line[i] == ','
+                    j += 1
+                    comma[j] = i
+                end
+            end
+            comma[end] = L+1
+            for i = 1:w
+                ps[k,i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+            end
+            k += 1
+        end
+    end
+    # The same for the Schlögl model
+    infile = "../Results/Fig3DataS/$(ARGS[2])paraS.csv"
+    if ~isfile(infile)
+        println("Error: No file of parameters to be read for Schlögl.")
+        return(nothing)
+    end
+    # now read in parameters
+    l = countlines(infile)
+    w = 4
+    psS = zeros(l,w)
+    open(infile, "r") do in_file
+        # Use a for loop to process the rows in the input file one-by-one
+        k = 1
+        for line in eachline(in_file)
+            # parse line by finding commas
+            L = length(line)
+            comma = fill(0,w+1)
+            j = 1
+            for i = 1:L
+                if line[i] == ','
+                    j += 1
+                    comma[j] = i
+                end
+            end
+            comma[end] = L+1
+            for i = 1:w
+                psS[k,i] = parse(Float64,line[(comma[i]+1):(comma[i+1]-1)])
+            end
+            k += 1
+        end
+    end
+    # Now need to read in trajectories
     return(nothing)
 end
 

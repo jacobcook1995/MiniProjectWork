99f036d4a4dcccfcf9be2165f7ed44e0bc30180d
Jacob Cook <jc2017@ic.ac.uk>
2019-11-25 14:05:09 +0000

    Both plots sorted

--- Code/PaperSup/epef.jl
+++ Code/PaperSup/epef.jl
@@ -10,6 +10,7 @@ using Plots
 using LaTeXStrings
 using Statistics
 using LsqFit
+using Plots.PlotMeasures
 import PyPlot
 
 # One plotting function in this script that should hopefully obtain everything needed
@@ -682,6 +683,96 @@ function comb()
         Sent[i,3] = Sents[i,1] # A
         Sent[i,4] = Sents[i,3] # B
     end
+    # Can find the EP and EF values from a file
+    # Make a structure to store the EP and EF terms for the toggle switch model
+    l = 100
+    ef = zeros(l,8)
+    datayn = fill(true,l)
+    for i = 1:l
+        for j = 1:2
+            # set file to read in
+            if j == 1
+                infile = "../Results/Fig3Data/Traj/$(i)$(ARGS[1])A2Bpf.csv"
+            else
+                infile = "../Results/Fig3Data/Traj/$(i)$(ARGS[1])B2Apf.csv"
+            end
+            # check it exits
+            if isfile(infile)
+                w = 4
+                open(infile, "r") do in_file
+                    # only one line now
+                    for line in eachline(in_file)
+                        # parse line by finding commas
+                        L = length(line)
+                        comma = fill(0,w+1)
+                        m = 1
+                        for k = 1:L
+                            if line[k] == ','
+                                m += 1
+                                comma[m] = k
+                            end
+                        end
+                        comma[end] = L+1
+                        for k = 1:w
+                            ef[i,(j-1)*4+k] = parse(Float64,line[(comma[k]+1):(comma[k+1]-1)])
+                        end
+                    end
+                end
+            else # if file doesn't exist inform user of missing data and exclude from plots
+                if j == 1
+                    println("Missing data for run $(i) high A to high B")
+                    datayn[i] = false
+                else
+                    println("Missing data for run $(i) high B to high A")
+                    datayn[i] = false
+                end
+            end
+        end
+    end
+    # Make a structure to store the EP and EF terms for the Schlögl model
+    Sef = zeros(l,8)
+    Sdatayn = fill(true,l)
+    for i = 1:l
+        for j = 1:2
+            # set file to read in
+            if j == 1
+                infile = "../Results/Fig3DataS/Traj/$(i)$(ARGS[2])h2lpf.csv"
+            else
+                infile = "../Results/Fig3DataS/Traj/$(i)$(ARGS[2])l2hpf.csv"
+            end
+            # check it exits
+            if isfile(infile)
+                w = 4
+                open(infile, "r") do in_file
+                    # only one line now
+                    for line in eachline(in_file)
+                        # parse line by finding commas
+                        L = length(line)
+                        comma = fill(0,w+1)
+                        m = 1
+                        for k = 1:L
+                            if line[k] == ','
+                                m += 1
+                                comma[m] = k
+                            end
+                        end
+                        comma[end] = L+1
+                        for k = 1:w
+                            Sef[i,(j-1)*4+k] = parse(Float64,line[(comma[k]+1):(comma[k+1]-1)])
+                        end
+                    end
+                end
+            else # if file doesn't exist inform user of missing data and exclude from plots
+                if j == 1
+                    println("Missing data for run $(i) high A to high B")
+                    Sdatayn[i] = false
+                else
+                    println("Missing data for run $(i) high B to high A")
+                    Sdatayn[i] = false
+                end
+            end
+        end
+    end
     # Now need to obtain the master equation switching data
     means = zeros(l,4)
     for i = 1:l
@@ -801,7 +892,7 @@ function comb()
         Smeans[i,4] = stdm(mastb,means[i,2];corrected=true)/sqrt(ne-1)
     end
     # Set up PyPlot
-    pyplot(dpi=200)
+    pyplot(dpi=200,margin=10mm)
     # First one to plot is the Schlögl model steady states
     p1 = scatter(Sent[:,1:2],Sent[:,3:4],label=[L"\dot{S}_A" L"\dot{S}_B"])
     plot!(xlabel="Exact (Schnakenberg)",ylabel="EP term",title="Steady state entropy productions Schlögl")
@@ -810,14 +901,23 @@ function comb()
     # Setup model
     @. model(x,p) = p[1] + p[2]*x
     # Choose data to fit model to
-    xdata = Sent[:,2] #[Sent[:,1]; Sent[:,2]]
-    ydata = Sent[:,4] #[Sent[:,3]; Sent[:,4]]
+    xdata = [Sent[:,1]; Sent[:,2]]
+    ydata = [Sent[:,3]; Sent[:,4]]
     # Set initial guess and plot range
     p0 = [0.0,1.0]
     xran = 0.0:1.0:63.0
     yint, slop, intlow, intup, r =  corrparr(xdata,ydata,p0,xran)
-    plot!(xran,model(xran,[yint,slop]),ribbon=(intlow,intup),label="",color=3)
-    println(r)
+    plot!(xran,model(xran,[yint,slop]),label="r = $(round(r,sigdigits=3))",color=3)
+    # Need to filter data before using it to fin annotation position
+    inr = falses(length(ydata))
+    for i = 1:length(inr)
+        if ydata[i] <= 1000.0
+            inr[i] = true
+        end
+    end
+    # Finally annotate
+    px, py = annpos(xdata[inr],ydata[inr])
+    annotate!(px,py,text("A",17,:black))
     # No data points to be excluded from the plot for toggle switch model
     p2 = scatter(ent[:,1:2],ent[:,3:4],label=[L"\dot{S}_A" L"\dot{S}_B"])
     plot!(xlabel="Exact (Schnakenberg)",ylabel="EP term",title="Steady state entropy productions toggle")
@@ -831,17 +931,52 @@ function comb()
     yint, slop, intlow, intup, r =  corrparr(xdata,ydata,p0,xran)
     # plot best fitting line => add correlation coefficient as a label
     plot!(xran,model(xran,[yint,slop]),label="r = $(round(r,sigdigits=3))",color=3)
-
-    p3 = plot()
-
-    p4 = plot()
+    # Finally annotate
+    px, py = annpos(collect(xran),collect(model(xran,[yint,slop])))
+    annotate!(px,py,text("B",17,:black))
+    # Now do Schlögl switching plot
+    p3 = plot(xlabel="EP term",ylabel="Exact (Gillespie)",title="Path entropy productions Schlögl")
+    # First plot one set
+    for i = 1:l
+        if i == 1
+            plot!([Sef[i,1]],[Smeans[i,1]],yerror=[Smeans[i,3]],label=L"\Delta S_{A{\rightarrow}B}",color=1)
+        else
+            plot!([Sef[i,1]],[Smeans[i,1]],yerror=[Smeans[i,3]],label="",color=1)
+        end
+    end
+    # and then the other
+    for i = 1:l
+        if i == 1
+            plot!([Sef[i,5]],[Smeans[i,2]],yerror=[Smeans[i,4]],label=L"\Delta S_{B{\rightarrow}A}",color=2)
+        else
+            plot!([Sef[i,5]],[Smeans[i,2]],yerror=[Smeans[i,4]],label="",color=2)
+        end
+    end
+    # The same for the toggle switch model
+    p4 = plot(xlabel="EP term",ylabel="Exact (Gillespie)",title="Path entropy productions toggle")
+    # First plot one set
+    for i = 1:l
+        if i == 1
+            plot!([ef[i,1]],[means[i,1]],yerror=[means[i,3]],label=L"\Delta S_{A{\rightarrow}B}",color=1)
+        else
+            plot!([ef[i,1]],[means[i,1]],yerror=[means[i,3]],label="",color=1)
+        end
+    end
+    # and then the other
+    for i = 1:l
+        if i == 1
+            plot!([ef[i,5]],[means[i,2]],yerror=[means[i,4]],label=L"\Delta S_{B{\rightarrow}A}",color=2)
+        else
+            plot!([ef[i,5]],[means[i,2]],yerror=[means[i,4]],label="",color=2)
+        end
+    end
+    
     # Finally combine plots
     plot(p1,p2,p3,p4,layout=(2,2),size=(1200,800))
     savefig("../Results/EPEF/test.png")
     return(nothing)
 end
 
-# MOVE THIS TO MY PLOTS SCRIPT WHEN I'M DONE
 # Function that takes sets of data and does some fitting
 # This returns the fit, the correlation coefficient, and the confidence interval
 function corrparr(xdata::Array{Float64,1},ydata::Array{Float64,1},p0::Array{Float64,1},xran::AbstractRange)
@@ -874,6 +1009,20 @@ function corrparr(xdata::Array{Float64,1},ydata::Array{Float64,1},p0::Array{Floa
     end
     r = a/sqrt(b*c)
     return(yint,slop,intlow,intup,r)
+end
+
+# A function to return positions for labels
+function annpos(datax::Array{Float64,1},datay::Array{Float64,1},δx=0.15::Float64,δy=0.125::Float64)
+    # Need minimas and maximas
+    xmax = maximum(datax)
+    xmin = minimum(datax)
+    # Calculate optimal x position for label
+    posx = xmin - δx*(xmax-xmin)
+    ymax = maximum(datay)
+    ymin = minimum(datay)
+    # Different formula for y as y axis extends a different length
+    posy = ymax + δy*(ymax-ymin)
+    return(posx,posy)
 end
 
 @time comb()
